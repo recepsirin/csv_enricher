9b99e463d1940d88b0cd9b302899a2c2d6409e65
recep <Recep.Sirin@protix.eu>
2024-05-20 19:11:13 +0200

    Add async file handling and CSV operations

---
+++ csv_enricher/functions.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import aiocsv
+import aiofiles
+from aiofiles import os
+
+
+async def get_files_in_directory(directory: str) -> list[str]:
+    files = await os.listdir(directory)
+    return files
+
+
+async def clean_temp_csv_files(directory: str) -> None:
+    """Delete all CSV files within a directory asynchronously."""
+    directory_path = Path(directory)
+
+    for item in directory_path.iterdir():
+        # If it's a file and has a .csv extension, remove it
+        if item.is_file() and item.suffix == ".csv":
+            await aiofiles.os.remove(item)
+
+
+async def split_csv_file(input_file: str, chunk_size: int) -> None:
+    async with aiofiles.open(input_file, "r", newline="") as infile:
+        reader = aiocsv.AsyncReader(infile)
+        headers = await reader.__anext__()  # Read the headers first
+
+        file_count = 1
+        while True:
+            output_file = f"_temp/chunks/{file_count}.csv"
+            async with aiofiles.open(output_file, "w", newline="") as outfile:
+                writer = aiocsv.AsyncWriter(outfile)
+                await writer.writerow(headers)  # Write the headers to each new file
+
+                # Write up to chunk_size rows
+                rows_written = 0
+                async for row in reader:
+                    await writer.writerow(row)
+                    rows_written += 1
+                    if rows_written >= chunk_size:
+                        break
+
+                if rows_written < chunk_size:
+                    return  # No more data to read or fewer rows than chunk_size left
+
+            file_count += 1
--- csv_enricher/main.py
+++ csv_enricher/main.py
@@ -9,6 +9,7 @@ from aiocsv import AsyncDictReader, AsyncDictWriter
 from aiofiles import os
 
 from csv_enricher.api_client import APIClient
+from csv_enricher.functions import clean_temp_csv_files, get_files_in_directory, split_csv_file
 
 
 async def read_csv_async(file_path: str) -> tuple[list[dict], list[str]]:
@@ -81,7 +82,16 @@ async def write_data_to_csv(file_path: str, data: list[dict[str, Any]]) -> None:
 
 
 async def main() -> None:
-    pass
+    file_to_be_enriched = ""
+    await split_csv_file(file_to_be_enriched, 500)  # so far most efficient option
+    files = await get_files_in_directory("_temp/chunks/")
+    sorted_files = sorted(files, key=lambda x: int(x.split(".")[0]))
+    for file in sorted_files:
+        rows_list, leis = await read_csv_async(f"_temp/chunks/{file}")
+        enriched_rows = await enrich(rows_list, leis)
+        await write_data_to_csv(".csv", enriched_rows)
+
+    await clean_temp_csv_files("_temp/chunks/")
 
 
 if __name__ == "__main__":
